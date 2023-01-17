1fa14fd10345c41eaaeeb66a67bf108cb9aa2307
Magda Gregorova <magda.gregorova@fhws.de>
2023-01-17 23:04:22 +0100

    one hot for made

--- hw1.py
+++ hw1.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# quick an dirty implementation for 2D inputs
 class MaskedLinear(nn.Linear):
     def __init__(self, in_features, out_features):
         super().__init__(in_features, out_features)
@@ -28,8 +29,23 @@ class MaskedOut(nn.Linear):
 class MADE(nn.Module):
     def __init__(self, n_hidden, d):
         super().__init__()
-        # self.l1 = MaskedLinear(2*d, n_hidden)
         self.l1 = MaskedLinear(2, n_hidden)
+        self.l11 = nn.Linear(n_hidden, n_hidden*2)
+        self.l12 = nn.Linear(n_hidden*2, n_hidden*2)
+        self.l2 = MaskedOut(n_hidden*2, 2*d)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l11(x))
+        x = F.relu(self.l12(x))
+        return self.l2(x)
+
+
+# quick an dirty implementation for onehot encoding
+class MADE_oh(nn.Module):
+    def __init__(self, n_hidden, d):
+        super().__init__()
+        self.l1 = MaskedLinear(2*d, n_hidden)
         self.l11 = nn.Linear(n_hidden, n_hidden*2)
         self.l12 = nn.Linear(n_hidden*2, n_hidden*2)
         self.l2 = MaskedOut(n_hidden*2, 2*d)
